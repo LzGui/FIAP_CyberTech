6b94b21a6c173e4e65680d29bdac48ce66756b8a
lzgui <lzguilhermecp@gmail.com>
2024-06-17 10:05:53 -0300

    Iniciando o projeto

---
+++ src/main/java/com/fiap/techchallengev/CybertechApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallengev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybertechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybertechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techchallengev/CybertechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallengev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybertechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
